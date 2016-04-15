4c8b8c486682c2492e59da89d4c7505d4a793a3e
John Whitley <j.n.whitley@gmail.com>
2016-04-15 16:04:42 +0100

    Refactored the binary json-csp-manager to be a class rather than a monolithic method

--- src/bin/json-csp-manager.js
+++ src/bin/json-csp-manager.js
@@ -8,47 +8,141 @@ import {JsonCspManager, JsonCspDocument} from '../index';
 cli.setUsage('app [options] json-csp-filename1 [json-csp-filename2 [json-csp-filename3 [...]]]');
 
 cli.parse({
-  table: ['t', 'Show tabular output of where the CSP rules came from'],
-  csp: ['c', 'Show the CSP response header']
+  csp: ['c', 'Show the CSP response header'],
+  json: ['j', 'Show the JSON CSP output'],
+  table: ['t', 'Show tabular output of where the CSP rules came from']
 });
 
-cli.main((args, options) => {
+/**
+ * Manages the CLI JSON CSP manager
+ */
+class JsonCspManagerCli {
 
-  const files = [];
+  /**
+   * Build the JsonCspManagerCli class
+   *
+   * @param {string[]} filenames the filenames to merge
+   * @param {object} options the options that were passed in by the CLI
+   */
+  constructor(filenames, options) {
+    /**
+     * @property {Promise[JsonCspDocument[]]} files the JsonCspDocument files to merge
+     */
+    this.files = [];
+    /**
+     * @property {object} options the options from the cli class
+     */
+    this.options = options;
 
-  args.forEach((filename) => {
-    files.push(
-      new Promise((resolve, reject) => {
-        fs.readFile(filename, 'utf-8', (err, data) => {
-          if (err) {
-            reject(err);
-          }
-
-          try {
-            const jsonCspDocument = new JsonCspDocument(data, filename);
-
-            if (jsonCspDocument instanceof JsonCspDocument) {
-              resolve(jsonCspDocument);
+    filenames.forEach((filename) => {
+      this.files.push(
+        new Promise((resolve, reject) => {
+          fs.readFile(filename, 'utf-8', (err, data) => {
+            if (err) {
+              reject(err);
             }
-          } catch (exception) {
-            reject(exception);
-          }
 
-          reject('unexpected error');
-        });
-      })
-    );
-  });
+            try {
+              const jsonCspDocument = new JsonCspDocument(data, filename);
 
+              if (jsonCspDocument instanceof JsonCspDocument) {
+                resolve(jsonCspDocument);
+              }
+            } catch (exception) {
+              reject(exception);
+            }
 
-  Promise.all(files).then((openedDocuments) => {
-    const manager = new JsonCspManager();
+            reject('unexpected error');
+          });
+        })
+      );
+    });
+  }
 
-    manager.jsonCspDocuments = openedDocuments;
+  /**
+   * Indicator that states whether the JSON output should be shown.
+   *
+   * @return {boolean} true if the JSON output should be shown.
+   */
+  get showJson() {
+    return this.options.json;
+  }
 
-    const mergedDocument = manager.mergedJsonCspDocument;
+  /**
+   * Indicator that states whether the CSP HTTP response header output should be shown.
+   *
+   * @return {boolean} true if the CSP HTTP response header output should be shown.
+   */
+  get showCsp() {
+    return this.options.csp;
+  }
 
-    if (options.table) {
+  /**
+   * Indicator that states whether the table explaining the merge should be shown.
+   *
+   * @return {boolean} true if the table explaining the merge should be shown.
+   */
+  get showTable() {
+    return this.options.table;
+  }
+
+  /**
+   * get all the JsonCspDocuments to be merged
+   *
+   * @return {Promise} the JsonCspDocuments
+   */
+  get jsonCspDocuments() {
+    return Promise.all(this.files);
+  }
+
+  /**
+   * get all JsonCspManager to manage the merging of the documents.
+   *
+   * @return {Promise} the JsonCspDocuments
+   */
+  get jsonCspManager() {
+    if (this.manager instanceof Promise) {
+      return this.manager;
+    }
+
+    this.manager = this.jsonCspDocuments.then((jsonCspDocuments) => {
+      const manager = new JsonCspManager();
+
+      manager.jsonCspDocuments = jsonCspDocuments;
+
+      return manager;
+    });
+
+    return this.manager;
+  }
+
+  /**
+   * get all JsonCspManager to manage the merging of the documents.
+   *
+   * @return {Promise} the JsonCspDocuments
+   */
+  get mergedDocument() {
+    return this.jsonCspManager.then((manager) => {
+      const mergedDocument = manager.mergedJsonCspDocument;
+
+      return mergedDocument;
+    });
+  }
+
+  /**
+   * get the CliTable of the explainations of the merge
+   *
+   * @return {CliTable} the table with the merge explanations in it
+   */
+  get cliTable() {
+    const promises = [
+      this.jsonCspManager,
+      this.mergedDocument
+    ];
+
+    return Promise.all(promises).then((resolvedPromises) => {
+      const manager = resolvedPromises.shift();
+      const mergedDocument = resolvedPromises.shift();
       const mergeReport = manager.jsonCspReport;
       const table = new CliTable({head: ['CSP Rule', 'Value', 'Included', 'File(s) it came from']});
 
@@ -66,24 +160,92 @@ cli.main((args, options) => {
             table.push(ruleDetails);
           });
         });
-
-
       });
 
-      const tableOutput = table.toString();
+      return table;
+    });
+  }
 
-      process.stdout.write(`${tableOutput}\n`);
-    }
-
-    if (options.csp) {
+  /**
+   * get the CSP HTTP response header value from the merged documents
+   *
+   * @return {string} the CSP HTTP response header value
+   */
+  get cspOutput() {
+    return this.mergedDocument.then((mergedDocument) => {
       const cspOutput = mergedDocument.toCsp();
 
-      process.stdout.write(`${cspOutput}\n`);
+      return cspOutput;
+    });
+  }
+
+  /**
+   * get the JSON CSP value from the merged documents
+   *
+   * @return {string} the JSON CSP value
+   */
+  get jsonOutput() {
+    return this.mergedDocument.then((mergedDocument) => {
+      const jsonOutput = mergedDocument.toJson();
+
+      return jsonOutput;
+    });
+  }
+
+
+  /**
+   * get all the required outputs
+   *
+   * @return {Promise} the string of all the outputs
+   */
+  get allOutputs() {
+    const outputPromises = [];
+
+    if (this.showTable) {
+      const tablePromise = this.cliTable.then((table) => {
+        const tableOutput = table.toString();
+
+        return `${tableOutput}\n`;
+      });
+
+      outputPromises.push(tablePromise);
     }
 
-  })
-  .catch((errorInJsonCspDocument) => {
-    process.stderr.write('Problem opening file: ', errorInJsonCspDocument.message);
-  });
+    if (this.showJson) {
+      const jsonPromise = this.jsonOutput;
 
+      outputPromises.push(jsonPromise);
+    }
+
+    if (this.showCsp) {
+      const cspPromise = this.cspOutput;
+
+      outputPromises.push(cspPromise);
+    }
+
+    return Promise.all(outputPromises).then((outputs) => {
+      const output = outputs.join(`\n`);
+
+      return output;
+    });
+  }
+
+  /**
+   * Display all the outputs on the terminal
+   *
+   * @return {void}
+   */
+  showAll() {
+    this.allOutputs.then((output) => {
+      process.stdout.write(`${output}\n`);
+    });
+
+    return;
+  }
+}
+
+cli.main((args, options) => {
+  const engine = new JsonCspManagerCli(args, options);
+
+  engine.showAll();
 });
